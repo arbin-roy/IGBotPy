8b2b2a16fc58097bdcee5a8f7b31cab3a74a8b23
arbin-roy <sroy45597@gmail.com>
2022-04-27 22:01:32 +0530

    webhook url added

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "Beautiful World"}
+def read_root(req: Request):
+    return {"Hello": "Beautiful World", "req": req.query_params['hub.challenge']}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/webhook")
+def webhook(req: Request):
+    print(req.query_params)
+    return int(req.query_params['hub.challenge'])
